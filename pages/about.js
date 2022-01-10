f08ba445786f57427b95286c770eab7e96ca91cd
JohanFre <70636307+JohanFre@users.noreply.github.com>
2022-01-10 13:12:12 +0100

    test

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
       <div>
         <h1>About</h1>
         <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
@@ -23,10 +23,10 @@ const About = () => {
           commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
           velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
           occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum."
+          mollit anim id est laborum.
         </p>
         <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
@@ -39,7 +39,7 @@ const About = () => {
           commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
           velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
           occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum."
+          mollit anim id est laborum.
         </p>
         <p></p>
       </div>
