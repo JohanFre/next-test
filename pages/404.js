ba1542aa9a65c70abd00389fa9fb1bf9f97e7e4d
JohanFre <70636307+JohanFre@users.noreply.github.com>
2022-01-10 13:23:08 +0100

    esesse

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import { useEffect } from "react";
-//import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     router.push("/");
-  //   }, 3000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
 
   return (
     <div className="not-found">
